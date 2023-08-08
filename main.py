c3099f47264fba1cc64960aae2948b889bf9601b
NEBULA1978 <rpascualey@gmail.com>
2023-08-08 20:23:28 +0200

    Tercera forma js y css enlazado añadido apuntes jinga2

--- main.py
+++ main.py
@@ -31,11 +31,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.templating import Jinja2Templates
 
 app=FastAPI()
 
 app.mount("/static",StaticFiles(directory="./static"),name="static")
+
+templates = Jinja2Templates(directory="./public/templates")
 
 
 # Ruta para archivos html
@@ -43,3 +45,9 @@ app.mount("/static",StaticFiles(directory="./static"),name="static")
 def root():
     html_address = "./static/html/index.html"
     return FileResponse(html_address,status_code=200)
+
+# Tercera forma:
+# Ruta para Jinja2
+@app.get("/template/{id}", response_class=HTMLResponse)
+def template(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})

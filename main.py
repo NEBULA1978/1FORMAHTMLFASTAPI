a580159ee9b00dd87d8729b0df73a05db0e6d022
NEBULA1978 <rpascualey@gmail.com>
2023-08-08 19:59:43 +0200

    Servimos un h1 segunda forma js y css enlazado

--- main.py
+++ main.py
@@ -10,21 +10,36 @@
 
 # SEGUNDA FORMA
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+# from fastapi import FastAPI
+# from fastapi.responses import HTMLResponse
+
+# app=FastAPI()
+
+# @app.get("/",response_class=HTMLResponse)
+# def root():
+#     return """
+#         <html>
+#         <head>
+#             <title>Web FastAPI</title>
+#         </head>
+#         <body>
+#             <h1>Hi I a, a html file</h1>
+#         </body>
+#     </html>
+
+#     """
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+
 
 app=FastAPI()
 
+app.mount("/static",StaticFiles(directory="./static"),name="static")
+
+
+# Ruta para archivos html
 @app.get("/",response_class=HTMLResponse)
 def root():
-    return """
-    <html>
-        <head>
-            <title>Web FastAPI</title>
-        </head>
-        <body>
-            <h1>Hi I a, a html file</h1>
-        </body>
-    </html>
-
-    """
+    html_address = "./static/html/index.html"
+    return FileResponse(html_address,status_code=200)
